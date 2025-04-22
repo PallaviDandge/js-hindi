9fd07fa809ee425d9fb09e277845369ff6c6e8b8
pallavi <96099056+PallaviDandge@users.noreply.github.com>
2025-04-22 06:40:36 +0000

    variable done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 1445553 
+// cant be change
+let accountEmail = "pbdandge666@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 2  //cant cahnge
+accountEmail = "pdandge8@gmail.com"
+accountPassword="123"
+accountCity="mumbai"   //not good way declare variable
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountCity,accountState])
