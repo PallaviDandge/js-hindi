cce398e3e99598f1cac81a85910a3d8c089ea947
pallavi <96099056+PallaviDandge@users.noreply.github.com>
2025-04-22 07:43:19 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
+
+//"33" => 33
+//"33abbc" = > NAN 
+//"true" => 1;false => 0
+
+
+let isLoggedIn = ""
+
+let boolenISLoggedIn = Boolean(isLoggedIn);
+
+console.log(boolenISLoggedIn)
+console.log( typeof boolenISLoggedIn)
+
+//1 => true 0=> false
+
+//""=> false
+//"pallavi" =>true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
